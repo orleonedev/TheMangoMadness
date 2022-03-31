0fea475a2263efb8a4676c725cc56401481cb985
Claudioios <claudiosilvestri48@yahoo.it>
2022-03-31 13:12:41 +0200

    Added buttons and buttons animation

--- TheMangoMadness/GameScene.swift
+++ TheMangoMadness/GameScene.swift
@@ -9,101 +9,111 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
+
+    private var backgroundButton : SKSpriteNode?
     
-    var entities = [GKEntity]()
-    var graphs = [String : GKGraph]()
+    private var blueButton : SKSpriteNode?
+    private var redButton : SKSpriteNode?
+    private var greenButton : SKSpriteNode?
     
-    private var lastUpdateTime : TimeInterval = 0
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    private var blueButtonAnimation: [SKTexture] = []
+    private var redButtonAnimation: [SKTexture] = []
+    private var greenButtonAnimation: [SKTexture] = []
+    
+    private var blueButtonGif : SKAction?
+    private var redButtonGif: SKAction?
+    private var greenButtonGif: SKAction?
+    
+    func blueAnimation(){
+        for i in 0...4 {
+            blueButtonAnimation.append(SKTexture(imageNamed: "blueButton\(i)"));
+        }
+        blueButtonGif = SKAction.animate(with: blueButtonAnimation, timePerFrame: 0.1)
+        blueButton?.run(blueButtonGif!)
+    }
+    
+    func redAnimation(){
+        for i in 0...4 {
+            redButtonAnimation.append(SKTexture(imageNamed: "redButton\(i)"));
+        }
+        redButtonGif = SKAction.animate(with: redButtonAnimation, timePerFrame: 0.1)
+        redButton?.run(redButtonGif!)
+
+    }
+    
+    func greenAnimation(){
+        
+        for i in 0...4 {
+            greenButtonAnimation.append(SKTexture(imageNamed: "greenButton\(i)"));
+        }
+        greenButtonGif = SKAction.animate(with: greenButtonAnimation, timePerFrame: 0.1)
+        greenButton?.run(greenButtonGif!)
+
+    }
     
     override func sceneDidLoad() {
 
-        self.lastUpdateTime = 0
+        backgroundButton = SKSpriteNode(color: .red, size: CGSize(width: self.size.width, height: self.size.height*0.2))
+        backgroundButton?.position = CGPoint(x: 0, y: -self.size.height*0.4)
+        backgroundButton?.zPosition = -10
+        addChild(backgroundButton!)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        blueButton = SKSpriteNode(texture: SKTexture(imageNamed: "blueButton0"), size: CGSize(width: backgroundButton!.size.width*0.2, height: backgroundButton!.size.height*0.4))
+        blueButton?.position = CGPoint(x: self.size.width*0.3, y: -self.size.height*0.4)
+        blueButton?.name = "blueButton"
+        addChild(blueButton!)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        redButton = SKSpriteNode(texture: SKTexture(imageNamed: "redButton0"), size: CGSize(width: backgroundButton!.size.width*0.2, height: backgroundButton!.size.height*0.4))
+        redButton?.position = CGPoint(x: 0, y: -self.size.height*0.4)
+        redButton?.zPosition = 1
+        redButton?.name = "redButton"
+        addChild(redButton!)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        greenButton = SKSpriteNode(texture: SKTexture(imageNamed: "greenButton0"), size: CGSize(width: backgroundButton!.size.width*0.2, height: backgroundButton!.size.height*0.4))
+        greenButton?.position = CGPoint(x: -self.size.width*0.3, y: -self.size.height*0.4)
+        greenButton?.name = "greenButton"
+        addChild(greenButton!)
     }
     
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
     }
     
     func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
+        let location = touches.first!.location(in: self)
+        let node = self.atPoint(location)
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
+        if (node.name == "redButton") {
+            redAnimation()
+        }
+        else{
+            if (node.name == "blueButton"){
+                blueAnimation()
+            }
+            else{
+                if (node.name == "greenButton"){
+                    greenAnimation()
+                }
+            }
+        }
     }
-    
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
     }
     
     
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
-        
-        // Initialize _lastUpdateTime if it has not already been
-        if (self.lastUpdateTime == 0) {
-            self.lastUpdateTime = currentTime
-        }
-        
-        // Calculate time since last update
-        let dt = currentTime - self.lastUpdateTime
-        
-        // Update entities
-        for entity in self.entities {
-            entity.update(deltaTime: dt)
-        }
-        
-        self.lastUpdateTime = currentTime
     }
 }
